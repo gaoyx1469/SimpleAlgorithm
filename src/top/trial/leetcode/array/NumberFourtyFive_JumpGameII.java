9bc5d81ebe21fb303c175405a3f6b77fdd7774cf
gaoyx1469 <Janner@192.168.1.103>
2019-12-18 21:06:33 +0800

    力扣45
    自己的解法与推荐解法

--- src/top/trial/leetcode/array/NumberFourtyFive_JumpGameII.java
+++ src/top/trial/leetcode/array/NumberFourtyFive_JumpGameII.java
@@ -28,16 +28,64 @@ import org.junit.Test;
 public class NumberFourtyFive_JumpGameII {
 	@Test
 	public void solution() {
-		int[] nums = {2,3,1,1,4};
+		int[] nums = { 2, 3, 1, 1, 4 };
 		int expected = 2;
-		
-		int result = getMinStep(nums);
-		
+
+		// int result = getMinStepOne(nums);
+		int result = getMinStepTwo(nums);
+
 		Assert.assertEquals(expected, result);
 	}
 
-	private int getMinStep(int[] nums) {
-		// TODO Auto-generated method stub
-		return 0;
+	/**
+	 * 自己想的算法，内存占用小，用时太长，时间复杂度O(n^2)，空间复杂度O(1)
+	 * 
+	 * @param nums
+	 * @return
+	 */
+	private int getMinStepOne(int[] nums) {
+
+		int nextStepIndex = nums.length - 1;
+		int stepNum = 0;
+
+		while (nextStepIndex > 0) {
+			nextStepIndex = getNextStep(nums, nextStepIndex);
+			stepNum++;
+		}
+
+		return stepNum;
 	}
+
+	// 获取能到达nextStepIndex处的最远点的下标并返回
+	private int getNextStep(int[] nums, int nextStepIndex) {
+		int farestIndex = nextStepIndex - 1;
+		for (int i = nextStepIndex - 1; i >= 0; i--) {
+			if (i + nums[i] >= nextStepIndex) {
+				farestIndex = i;
+			}
+		}
+		return farestIndex;
+	}
+
+	/**
+	 * 参考题解，从前往后的贪心算法，时间复杂度O(n),空间复杂复O(1)
+	 * 
+	 * @param nums
+	 * @return
+	 */
+	private int getMinStepTwo(int[] nums) {
+		int end = 0;
+		int maxPosition = 0;
+		int steps = 0;
+		for (int i = 0; i < nums.length - 1; i++) {
+			// 找能跳的最远的
+			maxPosition = Math.max(maxPosition, nums[i] + i);
+			if (i == end) { // 遇到边界，就更新边界，并且步数加一
+				end = maxPosition;
+				steps++;
+			}
+		}
+		return steps;
+	}
+
 }
