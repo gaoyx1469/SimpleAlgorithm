315fdb5ee30c6337d8d87d8f2233844163aa3387
gaoyx <gaoyx1469@163.com>
2020-05-21 10:37:19 +0800

    leetcode1371

--- src/top/trial/leetcode/string/Number1371_FindtheLongestSubstringContainingVowelsinEvenCounts.java
+++ src/top/trial/leetcode/string/Number1371_FindtheLongestSubstringContainingVowelsinEvenCounts.java
@@ -1,5 +1,7 @@
 package top.trial.leetcode.string;
 
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -45,12 +47,37 @@ public class Number1371_FindtheLongestSubstringContainingVowelsinEvenCounts {
 	/**
 	 * 没想到解法，参考官方题解
 	 * 
+	 * 前缀和+状态压缩
+	 * 
 	 * @param s
 	 * @return
 	 */
 	private int getLongestSubstringOne(String s) {
-		// TODO Auto-generated method stub
 
-		return 0;
+		int n = s.length();
+		int[] pos = new int[1 << 5];
+		Arrays.fill(pos, -1);
+		int ans = 0, status = 0;
+		pos[0] = 0;
+		for (int i = 0; i < n; i++) {
+			char ch = s.charAt(i);
+			if (ch == 'a') {
+				status ^= (1 << 0);
+			} else if (ch == 'e') {
+				status ^= (1 << 1);
+			} else if (ch == 'i') {
+				status ^= (1 << 2);
+			} else if (ch == 'o') {
+				status ^= (1 << 3);
+			} else if (ch == 'u') {
+				status ^= (1 << 4);
+			}
+			if (pos[status] >= 0) {
+				ans = Math.max(ans, i + 1 - pos[status]);
+			} else {
+				pos[status] = i + 1;
+			}
+		}
+		return ans;
 	}
 }
