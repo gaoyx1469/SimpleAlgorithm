000a279e5db36d3666732a9b22109ae53f6841b2
gaoyx <gaoyx1469@163.com>
2020-06-18 16:13:51 +0800

    treenode小修改

--- src/top/trial/leetcode/tree/TreeNode.java
+++ src/top/trial/leetcode/tree/TreeNode.java
@@ -3,13 +3,15 @@ package top.trial.leetcode.tree;
 /**
  * 二叉树结构定义类
  * 
+ * 属性增加public修饰，暂时忽略安全性，以保证其它包中的类可以访问
+ * 
  * @author Gaoyx
  *
  */
 public class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
 
 	TreeNode(int x) {
 		val = x;
