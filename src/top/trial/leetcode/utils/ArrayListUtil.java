c984b0a7266ade21e8584a9b0179e772c9827d3a
gaoyx <gaoyx1469@163.com>
2020-07-11 14:35:31 +0800

    泛型优化

--- src/top/trial/leetcode/utils/ArrayListUtil.java
+++ src/top/trial/leetcode/utils/ArrayListUtil.java
@@ -4,97 +4,96 @@ import java.util.List;
 
 /**
  * ArrayList工具类
- * 
- * @author Gaoyx-vm
  *
+ * @author Gaoyx-vm
  */
 
 public class ArrayListUtil {
 
-	/**
-	 * 对List进行一维输出
-	 * 
-	 * @param list
-	 * @return
-	 */
-	public static void getOneDimensionalSysoLikeArray(List<String> list) {
+    /**
+     * 对List进行一维输出
+     *
+     * @param list
+     * @return
+     */
+    public static <T> void getOneDimensionalSysoLikeArray(List<T> list) {
 
-		StringBuffer output = new StringBuffer();
-		Boolean isFirstElement = true;
+        StringBuffer output = new StringBuffer();
+        Boolean isFirstElement = true;
 
-		if (list == null || list.isEmpty())
-			output.append("[]");
-		else {
-			output.append("[");
-			for (String str : list) {
-				if (!isFirstElement) {
-					output.append(",");
-					isFirstElement = false;
-				}
-				output.append("\"").append(str).append("\"");
+        if (list == null || list.isEmpty())
+            output.append("[]");
+        else {
+            output.append("[");
+            for (T str : list) {
+                if (isFirstElement)
+                    isFirstElement = false;
+                else
+                    output.append(",");
+                output.append("\"").append(str.toString()).append("\"");
 
-			}
-			output.append("]");
-		}
+            }
+            output.append("]");
+        }
 
-		System.out.println(output);
-	}
+        System.out.println(output);
+    }
 
-	/**
-	 * 获取list的一维数组形式String
-	 * 
-	 * @param list
-	 * @return
-	 */
-	public static String getOneDimensionalStringLikeArray(List<String> list) {
+    /**
+     * 获取list的一维数组形式String
+     *
+     * @param list
+     * @return
+     */
+    public static <T> String getOneDimensionalStringLikeArray(List<T> list) {
 
-		StringBuffer output = new StringBuffer();
-		Boolean isFirstElement = true;
+        StringBuffer output = new StringBuffer();
+        Boolean isFirstElement = true;
 
-		if (list == null || list.isEmpty())
-			output.append("[]");
-		else {
-			output.append("[");
-			for (String str : list) {
-				if (!isFirstElement) {
-					output.append(",");
-					isFirstElement = false;
-				}
-				output.append("\"").append(str).append("\"");
+        if (list == null || list.isEmpty())
+            output.append("[]");
+        else {
+            output.append("[");
+            for (T str : list) {
+                if (isFirstElement)
+                    isFirstElement = false;
+                else
+                    output.append(",");
+                output.append("\"").append(str.toString()).append("\"");
 
-			}
-			output.append("]");
-		}
+            }
+            output.append("]");
+        }
 
-		return output.toString();
-	}
+        return output.toString();
+    }
 
-	/**
-	 * 对List进行二维输出
-	 * 
-	 * @param list
-	 * @return
-	 */
-	public static void getTwoDimensionalSysoLikeArray(List<List<String>> lists) {
+    /**
+     * 对List进行二维输出
+     *
+     * @param lists
+     * @return
+     */
+    public static <T> void getTwoDimensionalSysoLikeArray(List<List<T>> lists) {
 
-		Boolean isFirstElement = true;
+        Boolean isFirstElement = true;
 
-		if (lists == null || lists.isEmpty())
-			System.out.println("[]");
-		else {
-			System.out.println("[");
-			for (List<String> list : lists) {
+        if (lists == null || lists.isEmpty())
+            System.out.println("[]");
+        else {
+            System.out.println("[");
+            for (List<T> list : lists) {
 
-				if (isFirstElement)
-					isFirstElement = false;
-				else
-					System.out.println(",");
-				System.out.print(" " + getOneDimensionalStringLikeArray(list));
-			}
-			System.out.println();
-			System.out.println("]");
-		}
+                if (isFirstElement)
+                    isFirstElement = false;
+                else
+                    System.out.println(",");
+                System.out.print(" " + getOneDimensionalStringLikeArray(list));
+            }
+            System.out.println();
+            System.out.println("]");
+        }
 
-	}
+    }
 
 }
