35279d150f9dba40baf74fb8dca670b16f8a4550
gaoyx <gaoyx1469@163.com>
2020-07-21 13:41:30 +0800

    leetcode783

---
+++ src/top/trial/leetcode/tree/Number0783_MinimumDistanceBetweenBSTNodes.java
@@ -0,0 +1,46 @@
+package top.trial.leetcode.tree;
+
+import org.junit.Test;
+
+/**
+ * 给定一个二叉搜索树的根节点 root，返回树中任意两节点的差的最小值。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 输入: root = [4,2,6,1,3,null,null]
+ * 输出: 1
+ * 解释:
+ * 注意，root是树节点对象(TreeNode object)，而不是数组。
+ * <p>
+ * 给定的树 [4,2,6,1,3,null,null] 可表示为下图:
+ * <p>
+ * 4
+ * /   \
+ * 2      6
+ * / \
+ * 1   3
+ * <p>
+ * 最小的差值是 1, 它是节点1和节点2的差值, 也是节点3和节点2的差值。
+ *  
+ * <p>
+ * 注意：
+ * <p>
+ * 二叉树的大小范围在 2 到 100。
+ * 二叉树总是有效的，每个节点的值都是整数，且不重复。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author gaoyx1469
+ * @Date 2020/7/21
+ * @Version 1.0
+ */
+public class Number0783_MinimumDistanceBetweenBSTNodes {
+    @Test
+    public void solution() {
+        //本题与530题一样，不再重复解答
+    }
+}
