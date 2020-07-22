911c8becbb4a7e4c570770598463e448c9cb250b
gaoyx <gaoyx1469@163.com>
2020-07-22 11:47:45 +0800

    leetcode154
    跟F011一样

---
+++ src/top/trial/leetcode/array/Number0154_FindMinimumInRotatedSortedArrayII.java
@@ -0,0 +1,43 @@
+package top.trial.leetcode.array;
+
+import org.junit.Test;
+
+/**
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ * <p>
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ * <p>
+ * 请找出其中最小的元素。
+ * <p>
+ * 注意数组中可能存在重复的元素。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: [1,3,5]
+ * 输出: 1
+ * 示例 2：
+ * <p>
+ * 输入: [2,2,2,0,1]
+ * 输出: 0
+ * 说明：
+ * <p>
+ * 这道题是 寻找旋转排序数组中的最小值 的延伸题目。
+ * 允许重复会影响算法的时间复杂度吗？会如何影响，为什么？
+ * <p>
+ * <p>
+ * 本题与F011一样，请参考NumberF011_LCOF
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author gaoyx1469
+ * @Date 2020/7/22
+ * @Version 1.0
+ */
+public class Number0154_FindMinimumInRotatedSortedArrayII {
+    @Test
+    public void solution() {
+
+    }
+}
