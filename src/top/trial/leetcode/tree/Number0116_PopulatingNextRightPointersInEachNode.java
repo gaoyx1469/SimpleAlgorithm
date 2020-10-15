a88460350664674904967717c5d996a25e2cdd96
gaoyx <gaoyx1469@163.com>
2020-10-15 09:50:07 +0800

    leetcode116

---
+++ src/top/trial/leetcode/tree/Number0116_PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,70 @@
+package top.trial.leetcode.tree;
+
+import org.junit.Test;
+
+/**
+ * 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+ * <p>
+ * struct Node {
+ * int val;
+ * Node *left;
+ * Node *right;
+ * Node *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * <p>
+ * 示例：
+ * 输入：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":null,"right":null,"val":4},"next":null,"right":{"$id":"4","left":null,"next":null,"right":null,"val":5},"val":2},"next":null,"right":{"$id":"5","left":{"$id":"6","left":null,"next":null,"right":null,"val":6},"next":null,"right":{"$id":"7","left":null,"next":null,"right":null,"val":7},"val":3},"val":1}
+ * 输出：{"$id":"1","left":{"$id":"2","left":{"$id":"3","left":null,"next":{"$id":"4","left":null,"next":{"$id":"5","left":null,"next":{"$id":"6","left":null,"next":null,"right":null,"val":7},"right":null,"val":6},"right":null,"val":5},"right":null,"val":4},"next":{"$id":"7","left":{"$ref":"5"},"next":null,"right":{"$ref":"6"},"val":3},"right":{"$ref":"4"},"val":2},"next":null,"right":{"$ref":"7"},"val":1}
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+ *  
+ * 提示：
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author gaoyx1469
+ * @Date 2020/10/15
+ * @Version 1.0
+ */
+public class Number0116_PopulatingNextRightPointersInEachNode {
+    @Test
+    public void solution() {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        connect(root);
+    }
+
+    /**
+     * 由先做的117题的启发，使用构建好的上层的next指针即可方便地按层遍历
+     * 且该题为满二叉树，因此比117更方便，少了一层判断
+     *
+     * @param root Node
+     * @return Node
+     */
+    private Node connect(Node root) {
+        Node high = root;
+        if (high == null)
+            return null;
+        while (high.left != null) {
+            Node first = high;
+            while (high.next != null) {
+                high.left.next = high.right;
+                high.right.next = high.next.left;
+                high = high.next;
+            }
+            high.left.next = high.right;
+            high = first.left;
+        }
+        return root;
+    }
+}
