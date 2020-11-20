caa07f96039541562354cd3fe0476344f6b14f25
gaoyx <gaoyx1469@163.com>
2020-11-20 10:45:56 +0800

    leetcode147

---
+++ src/top/trial/leetcode/linkedList/Number0147_InsertionSortList.java
@@ -0,0 +1,61 @@
+package top.trial.leetcode.linkedList;
+
+import org.junit.Test;
+
+/**
+ * 插入排序算法：
+ * 插入排序是迭代的，每次只移动一个元素，直到所有元素可以形成一个有序的输出列表。
+ * 每次迭代中，插入排序只从输入数据中移除一个待排序的元素，找到它在序列中适当的位置，并将其插入。
+ * 重复直到所有输入数据插入完为止。
+ * <p>
+ * 示例 1：
+ * 输入: 4->2->1->3
+ * 输出: 1->2->3->4
+ * 示例 2：
+ * 输入: -1->5->3->4->0
+ * 输出: -1->0->3->4->5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/insertion-sort-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author gaoyx1469
+ * @Date 2020/11/20
+ * @Version 1.0
+ */
+public class Number0147_InsertionSortList {
+    @Test
+    public void solution() {
+        ListNode head = new ListNode(-1);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(0);
+        head = insertionSortList(head);
+        System.out.println(LinkedListUtils.goThroughLinkedList(head));
+    }
+
+    private ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode lastSorted = head, curr = head.next;
+        while (curr != null) {
+            if (lastSorted.val <= curr.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode prev = dummyHead;
+                while (prev.next.val <= curr.val) {
+                    prev = prev.next;
+                }
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+        return dummyHead.next;
+    }
+}
