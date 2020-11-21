5596974adfc1b97c01da12b97cff9fb83fb968c4
gaoyx <gaoyx1469@163.com>
2020-11-21 10:59:19 +0800

    leetcode148

---
+++ src/top/trial/leetcode/linkedList/Number0148_SortList.java
@@ -0,0 +1,82 @@
+package top.trial.leetcode.linkedList;
+
+import org.junit.Test;
+
+
+/**
+ * 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+ * 进阶：
+ * 你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+ * <p>
+ * 提示：
+ * 链表中节点的数目在范围 [0, 5 * 10^4] 内
+ * -10^5 <= Node.val <= 10^5
+ *
+ * @Author gaoyx1469
+ * @Date 2020/11/21
+ * @Version 1.0
+ */
+public class Number0148_SortList {
+    @Test
+    public void solution() {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(3);
+        head = sortList(head);
+        System.out.println(LinkedListUtils.goThroughLinkedList(head));
+    }
+
+    /**
+     * O(n log n)不就是快排、归并和堆？使用归并最合适
+     *
+     * @param head ListNode
+     * @return ListNode
+     */
+    private ListNode sortList(ListNode head) {
+        return sortList(head, null);
+    }
+
+    public ListNode sortList(ListNode head, ListNode tail) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == tail) {
+            head.next = null;
+            return head;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != tail) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != tail) {
+                fast = fast.next;
+            }
+        }
+        ListNode mid = slow;
+        ListNode list1 = sortList(head, mid);
+        ListNode list2 = sortList(mid, tail);
+        return merge(list1, list2);
+    }
+
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 != null) {
+            temp.next = temp1;
+        } else if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
+    }
+}
